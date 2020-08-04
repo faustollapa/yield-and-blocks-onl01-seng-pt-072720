a652a45126110363fa859955622df351a86e5660
Fausto Llapa <faustollapa850@gmail.com>
2020-08-04 16:46:00 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -5,14 +5,13 @@ def hello_t(array)
       i = i + 1
     end
     array
-end
+end    
+  else
+    puts "Hey! No block was given!"
+  end  
+end     
 
 
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
 
 
 
